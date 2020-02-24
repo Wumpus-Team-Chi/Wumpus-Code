f9c004dc3e972e68832138957188d4ac7cb16001
pratyush1718 <54123007+pratyush1718@users.noreply.github.com>
2020-02-24 11:50:30 -0800

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Wumpus_Test;
+
+namespace WumpusTest
+{
+    public partial class Form1 : Form
+    {
+        private GameControl gameControl;
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            gameControl = new GameControl();
+            MessageBox.Show("The game control class has been constructed!");
+            ConstructorButton.Enabled = false;
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            gameControl.newGame();
+            MessageBox.Show("GameControl has begun new game\n Welcome to Hunt The Wumpus: Space Edition!", "Message:");
+            NewGameButton.Enabled = false;
+
+        }
+    }
+
+
+    }
+
