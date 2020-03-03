b2097e196743cf203aed1d5ec4d5e4651d91bfab
s-syadav <61330167+s-syadav@users.noreply.github.com>
2020-03-02 21:53:42 -0800

    Update Cave.cs

--- HuntTheWumpus/Cave.cs
+++ HuntTheWumpus/Cave.cs
@@ -40,6 +40,48 @@ namespace HuntTheWumpus
         //mutators
             //cave will remain static throughout. 
         //accesors
+        public int[] getBatRooms()
+        {
+            int room1 = -1;
+            int room2 = -2;
+            int[] batRoomNumbers = new int[2];
+            for (int i = 0; i < myCave.Length; i++)
+            {
+                for (int j = 0; j < myCave[i].Length; j++)
+                {
+                    int r = myCave[i][j].getRoomNum();  
+                    if (myCave[i][j].isBatPresent(r))
+                    {
+                        if (room1 != room2 && room1 < 0)
+                        {
+                            room1 = myCave[i][j].getRoomNum();
+                        }else if(room2 < 0)
+                        {
+                            room2 = myCave[i][j].getRoomNum();
+                        }
+                    }
+                }
+            }
+            batRoomNumbers[0] = room1;
+            batRoomNumbers[1] = room2;
+            return batRoomNumbers; 
+        }
+        public int getWumpusRoom()
+        {
+        
+            for (int i = 0; i < myCave.Length; i++)
+            {
+                for (int j = 0; j < myCave[i].Length; j++)
+                {
+                    int r = myCave[i][j].getRoomNum();
+                    if (myCave[i][j].isWumpusPresent(r))
+                    {
+                        return r;
+                    }
+                }
+            }
+            return -1; // if it gets here something in cave generation or wumpus assignment went wrong. 
+        }
         public int[] adjacentRooms()
         {
             int[] adjacentRooms = { 1, 2, 3, 4, 5, 6 }; // PLACEHOLDER 
